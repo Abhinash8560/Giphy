8a2e15c5a7cc77c09f41b05a4bd6d0abe72eb178
Abhinash kumar <avinashk9471@gmail.com>
2021-11-12 18:30:47 +0530

    firstCommit

--- src/Components/Giphy.js
+++ src/Components/Giphy.js
@@ -1,38 +1,14 @@
-import React, {useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const Giphy = () => {  
+import Loader from "./Loader";
+import Paginate from "./Paginate";
 
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-          setIsLoading(true);
-          const results = await axios("https://api.giphy.com/v1/gifs/trending", {
-            params: {
-              api_key: "tAEFUgagRjRNkU24orQdFB8EHMcNTUSe",
-              limit: 1000
-            }
-        });
-        console.log(results);
-        setData(results.data.data);
 
-        setIsLoading(false);
-    };
-    fetchData();
-}, []);
-const renderGifs = () => {
-    if (isLoading) {
-      return <Loader />;
-    }
-    return currentItems.map(el => {
-      return (
-        <div key={el.id} className="gif">
-          <img src={el.images.fixed_height.url} />
-        </div>
-      );
-    });
-  };   
     
-export default Giphy
+ 
+
+    
+
+export default Giphy;
